fe0421b7b694815925fe0ca90d2fecc41ea1c847
sjouke <sjouke@0fbddf0c-6d10-0410-a391-d785bef39c56>
2009-06-09 19:18:02 +0000

    Added missing config.php to the branch

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Main configuration file required by all other files in GM
+ *
+ * The variables in this file are the main configuration variable for the site
+ * Gedcom specific configuration variables are stored in the config_gedcom.php file.
+ * Site administrators may edit these settings online through the editconfig.php file.
+ *
+ * Genmod: Genealogy Viewer
+ * Copyright (C) 2005 Genmod Development Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package Genmod
+ * @subpackage Admin
+ * @see editconfig.php
+ * @version $Id$
+ */
+
+require_once("includes/session.php");
+?>
