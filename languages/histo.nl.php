a441c230c049ddf379d67c384dfcfa5d6022ba30
sjouke <sjouke@0fbddf0c-6d10-0410-a391-d785bef39c56>
2010-04-11 06:21:35 +0000

    Added empty language files for historical facts

---
+++ languages/histo.nl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * File used to display Historical facts on individual page
+ *
+ * Each line is a GEDCOM style record to describe an event, including newline chars (\n)
+ * File to be renamed : histo.xx.php where xx is language code
+ *
+ * $Id$
+ */
+// Voorbeeld van formaat:
+//$histo[] = "1 EVEN\n2 TYPE _HIST\n2 DATE 10 MAY 1940\n2 NOTE Begin Tweede Wereldoorlog";
+?>
