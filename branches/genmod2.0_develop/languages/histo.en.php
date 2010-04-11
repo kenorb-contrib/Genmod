d1c7100403deb5ed42c0a89cc8290d372f2d7235
sjouke <sjouke@0fbddf0c-6d10-0410-a391-d785bef39c56>
2010-04-11 06:21:35 +0000

    Added empty language files for historical facts

---
+++ branches/genmod2.0_develop/languages/histo.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * File used to display Historical facts on individual page
+ *
+ * Each line is a GEDCOM style record to describe an event, including newline chars (\n)
+ * File to be renamed : histo.xx.php where xx is language code
+ *
+ * $Id$
+ */
+// Example of format:
+//$histo[] = "1 EVEN\n2 TYPE _HIST\n2 DATE 8 MAY 1945\n2 NOTE WW2 Armistice";
+?>
