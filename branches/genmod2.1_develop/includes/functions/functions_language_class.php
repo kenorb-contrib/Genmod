9f0ea13cf3c8ce88f9eaf22f6bec960284da6f9e
sjouke <sjouke@0fbddf0c-6d10-0410-a391-d785bef39c56>
2020-11-10 11:12:04 +0000

    - Fixed error when printing help text that does not exist

--- branches/genmod2.1_develop/includes/functions/functions_language_class.php
+++ branches/genmod2.1_develop/includes/functions/functions_language_class.php
@@ -270,7 +270,7 @@ abstract class LanguageFunctions {
 	 * @return 	string	The string in the requested language
 	 */
 	public function GetString($string, $language2, $file_type="") {
-	
+
 		if ($file_type == "facts") {
 			$sql = "SELECT lg_".$language2.", lg_english FROM ".TBLPREFIX."facts";
 			$sql .= " WHERE lg_string = '".$string."'";
@@ -283,7 +283,8 @@ abstract class LanguageFunctions {
 	
 		$res = NewQuery($sql);
 		$row = $res->FetchAssoc($res->result);
-		if (empty($row["lg_".$language2])) return $row["lg_english"];
+		if ($res->NumRows($res) == 0) return "";
+		else if (empty($row["lg_".$language2])) return $row["lg_english"];
 		else return $row["lg_".$language2];
 	
 	}
